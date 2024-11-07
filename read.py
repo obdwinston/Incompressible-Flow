f0ce21b99e2368ad1363d65b3653ba5f7adae61d
obdwinston <obdwinston@gmail.com>
2024-11-07 14:48:04 +0800

    minor changes

--- read.py
+++ read.py
@@ -6,7 +6,7 @@ from scipy.interpolate import griddata
 xb = [1., 4.]       # grid x-bounds
 yb = [1., 2.]       # grid y-bounds
 fsz = [9, 3]       # figure size
-res = 400           # grid resolution
+res = 200           # grid resolution
 fps = 100           # frames per second
 
 # read configuration
